f179cc2e6fc142bc7aea8eb3e981c03a5a56901d
Harmik Batth <hsbatth@outlook.com>
2026-03-24 22:00:54 +1100

    new code 1

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e0d5a86220105042d5543a9782499a20d5d387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdfffbfe1fb2caf6125b2b639357d3943695c7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
